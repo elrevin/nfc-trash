8d876d64fd1cc6791040330dd6138c7b40151d43
elrevin <elrevin@gmail.com>
2017-09-22 00:05:16 +0500

    Trying to read nfs tag content

--- app/src/main/java/me/elrevin/nfc_trash/MainActivity.java
+++ app/src/main/java/me/elrevin/nfc_trash/MainActivity.java
@@ -3,10 +3,18 @@ package me.elrevin.nfc_trash;
 import android.app.PendingIntent;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.nfc.NdefMessage;
+import android.nfc.NdefRecord;
 import android.nfc.NfcAdapter;
+import android.nfc.Tag;
+import android.os.Parcelable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
 import android.widget.Toast;
+
+import java.io.UnsupportedEncodingException;
 
 public class MainActivity extends AppCompatActivity {
     NfcAdapter nfcAdapter;
@@ -25,9 +33,16 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onNewIntent(Intent intent) {
-        Toast.makeText(this, "Nfc intent received", Toast.LENGTH_LONG).show();
-
         super.onNewIntent(intent);
+
+        if (intent.hasExtra(NfcAdapter.EXTRA_TAG)) {
+            Parcelable[] parcelables = intent.getParcelableArrayExtra(NfcAdapter.EXTRA_NDEF_MESSAGES);
+            if (parcelables != null && parcelables.length > 0) {
+                readTextFromNdefMessage((NdefMessage) parcelables[0]);
+            } else {
+                Toast.makeText(this, "No NFC messages found", Toast.LENGTH_SHORT).show();
+            }
+        }
     }
 
     @Override
@@ -46,5 +61,30 @@ public class MainActivity extends AppCompatActivity {
         super.onPause();
 
         nfcAdapter.disableForegroundDispatch(this);
+    }
+
+    public String getTextFromNdefRecord(NdefRecord record) {
+        String tagContent = null;
+        try {
+            byte[] payload = record.getPayload();
+            String textEncoding = ((payload[0] & 128) == 0) ? "UTF-8" : "UTF16";
+            int languageSize = payload[0] & 0063;
+            tagContent = new String(payload, languageSize - 1, payload.length - languageSize - 1, textEncoding);
+        } catch (UnsupportedEncodingException e) {
+            Log.e("Nfc trash error", e.getMessage(), e);
+        }
+
+        return tagContent;
+    }
+
+    public void readTextFromNdefMessage(NdefMessage message) {
+        NdefRecord[] records = message.getRecords();
+        if (records != null && records.length > 0) {
+            NdefRecord record = records[0];
+            String text = getTextFromNdefRecord(record);
+            ((TextView) findViewById(R.id.tvTag)).setText(text);
+        } else {
+            Toast.makeText(this, "No NFC records found", Toast.LENGTH_SHORT).show();
+        }
     }
 }
